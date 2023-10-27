4bb63fd8178f8b5bdaa1e99664e98b44bb25185a
arce <Joo Augusto>
2023-10-27 17:43:06 -0300

    configuracoes de deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Contact } from "./components/Contact";
-import { Header } from "./components/Header";
-import { Hero01 } from "./components/Hero01";
-import { Services } from "./components/Services";
+// import { Contact } from "./components/Contact";
+// import { Header } from "./components/Header";
+// import { Hero01 } from "./components/Hero01";
+// import { Services } from "./components/Services";
 
 function App() {
   return (
     <>
       <main className="container min-w-[360px] h-full max-w-5xl p-5 mx-auto bg-neutral-50">
-        <Header />
-        <Hero01 />
-        <Services />
-        <Contact />
+        {/* <Header /> */}
+        {/* <Hero01 /> */}
+        {/* <Services /> */}
+        {/* <Contact /> */}
+        <h1>teste</h1>
       </main>
     </>
   );
